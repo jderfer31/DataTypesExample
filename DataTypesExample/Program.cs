af7b4d7b14e2ad5e80b9c8df3920d01129660c4d
jderfer0979@gmail.com <jderfer0979@gmail.com>
2020-06-01 18:02:51 -0400

    Add project files.

---
+++ DataTypesExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DataTypesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable is just a container where we can store information and there are different types of data as I will see
+            //string phrase = "";  //this is a string of text - with double quote
+            //char grade = ''; //This allows for only 1 single character - single quote
+            //int age = 30; //Quotes aren't needed for integers - side note, float is least accurate, double and most accurate is decimal
+            //double gpa = 3.2;
+            //bool isMale = true; //or false //boolean is a true or false value
+
+
+            Console.WriteLine("Test");
+
+
+
+            
+        }
+    }
+}
